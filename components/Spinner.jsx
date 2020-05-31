27d9af4adb276f794988d03dc1418a8c64204b48
Lukas Gabsi <lukas.gabsi@towa.at>
2020-05-31 17:48:43 +0200

    updated Spinner styles

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,26 +1,59 @@
 import styled, { keyframes } from 'styled-components';
 
-const rotate360 = keyframes`
+const pulseSize = '96px';
+const pulseColor = '#18466E';
+const pulseDuration = '2s';
+
+const pulse = keyframes`
   from {
-    transform: rotate(0deg);
+    width: 0px;
+    height: 0px;
+    opacity: 0;
   }
-  to {
-    transform: rotate(360deg);
+  4% {
+    opacity: 0;
+  }
+  20% {
+    opacity: 1;
+  }
+  70% {
+    width: ${pulseSize};
+    height: ${pulseSize};
+    margin-left: -${pulseSize} / 2;
+    margin-top: -${pulseSize} / 2;
+    opacity: 0;
   }
 `;
 
-const Spinner = styled.div`
-  animation: ${rotate360} 1s linear infinite;
-  transform: translateZ(0);
+const LoadingPulse = styled.div`
+  height: ${pulseSize};
+  width: ${pulseSize};
+`;
 
-  border-top: 2px solid grey;
-  border-right: 2px solid grey;
-  border-bottom: 2px solid grey;
-  border-left: 4px solid black;
-  background: transparent;
-  width: 24px;
-  height: 24px;
+const Circle = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  border: 1px solid ${pulseColor};
   border-radius: 50%;
+  opacity: 0;
+  will-change: opacity;
+
+  &:nth-child(1) {
+    animation: ${pulse} ${pulseDuration} 0.3s infinite;
+  }
+
+  &:nth-child(2) {
+    animation: ${pulse} ${pulseDuration} 0.6s infinite;
+  }
 `;
+
+const Spinner = () => (
+  <LoadingPulse>
+    <Circle />
+    <Circle />
+  </LoadingPulse>
+);
 
 export default Spinner;
