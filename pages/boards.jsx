9c6f73554da3a7b0c2b9b337dcbf0064e6db301a
Lukas Gabsi <lukas.gabsi@towa.at>
2020-07-26 12:41:27 +0200

    fixed an error where favourite boards weren't instantiated correctly

--- pages/boards.jsx
+++ pages/boards.jsx
@@ -37,7 +37,7 @@ const Boards = ({ boards, user, authToken, favourites }) => {
   const [search, setSearch] = useState('');
 
   const toggleFavourite = board => {
-    if (favouriteBoards.includes(board)) {
+    if (favouriteBoards.some(fav => fav.id === board.id)) {
       fetch(`/api/boards/${board.id}/favourite`, {
         method: 'DELETE',
         body: JSON.stringify({ user }),
