96cfb8470b907cd59e7cb2ed8557d67a63f3dadf
Lukas Gabsi <lukas.gabsi@towa.at>
2020-08-15 10:06:44 +0200

    dirty hack to fix linting stuff

--- utils/hooks/useColumns.js
+++ utils/hooks/useColumns.js
@@ -1,31 +1,45 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
 import { useMemo } from 'react';
 import { format } from 'date-fns';
+import { Text } from '@zeit-ui/react';
+import PropTypes from 'prop-types';
+
+const Header = ({ column, text }) => (
+  <Text span i={column.isSorted}>
+    {text}
+  </Text>
+);
+
+Header.propTypes = {
+  column: PropTypes.object,
+  text: PropTypes.string,
+};
 
 const useColumns = () =>
   useMemo(
     () => [
       {
-        Header: 'Sprint Name',
+        Header: ({ column }) => <Header column={column} text="Sprint Name" />,
         accessor: 'name',
       },
       {
-        Header: 'Start Date',
+        Header: ({ column }) => <Header column={column} text="Start Date" />,
         id: 'startDate',
         accessor: originalRow =>
           format(new Date(originalRow.startDate), 'dd-MM-yyyy'),
       },
       {
-        Header: 'End Date',
+        Header: ({ column }) => <Header column={column} text="End Date" />,
         id: 'endDate',
         accessor: originalRow =>
           format(new Date(originalRow.endDate), 'dd-MM-yyyy'),
       },
       {
-        Header: 'Forecast',
+        Header: ({ column }) => <Header column={column} text="Forecast" />,
         accessor: 'forecast',
       },
     ],
     [],
   );
-
 export default useColumns;
