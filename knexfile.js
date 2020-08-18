49ca886c7c8af3ca9e977af825eae5258cee6934
Lukas Gabsi <lukas.gabsi@towa.at>
2020-08-18 23:31:41 +0200

    vercel test

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,12 @@
 // Update with your config settings.
 
+console.log({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+});
+
 module.exports = {
   development: {
     client: 'mysql',
