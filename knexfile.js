7f5bd2099be4167087ed3c331356cbf08d623c27
Lukas Gabsi <lukas.gabsi@towa.at>
2020-08-23 20:22:03 +0200

    loginform button is now a submit button

--- knexfile.js
+++ knexfile.js
@@ -18,15 +18,12 @@ module.exports = {
   },
   production: {
     client: 'mysql',
+    version: '5.7',
     connection: {
       host: process.env.DATABASE_HOST,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
-    },
-    pool: {
-      min: 0,
-      max: 10,
     },
     migrations: {
       tableName: 'knex_migrations',
