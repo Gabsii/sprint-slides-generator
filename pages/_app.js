e4047b4661980c934a605d5e84bd4c08eae8c05c
Lukas Gabsi <lukas.gabsi@towa.at>
2020-08-30 11:29:07 +0200

    fixed a css bug where slides wouldn't be visible

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: transparent;
   }
   * {
     box-sizing: border-box;
