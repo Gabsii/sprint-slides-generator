69848d7437a11201f76d51b94269a74a4a5cea24
Lukas Gabsi <lukas.gabsi@towa.at>
2020-09-19 00:18:14 +0200

    added head tag to 404

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,14 @@
 import { Image, Link, Page, Row, Text } from '@zeit-ui/react';
+import Head from 'next/head';
 import NextLink from 'next/link';
 
 const Custom404 = () => (
   <Page>
+    <Head>
+      <title>404 - Page Not Found | SprintGenerator</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Page.Content>
       <Text h1>404 - Page Not Found</Text>
       <Text h2>Looks like you went out of your boundaries!</Text>
