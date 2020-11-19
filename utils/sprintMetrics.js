899127fd78c59e08bc51c05b78365126ca718a90
Lukas Gabsi <lukas.gabsi@towa.at>
2020-11-19 10:08:17 +0100

    even more stories

--- utils/sprintMetrics.js
+++ utils/sprintMetrics.js
@@ -3,14 +3,15 @@ export const storiesDone = (stories) =>
   Object.values(stories).reduce((acc, curr) => (acc += curr.length), 0);
 
 export const completedStoryPoints = (stories, bugs, others) =>
-  Object.values(stories).reduce(
-    (acc, curr) =>
-      (acc += curr.reduce(
-        (newAcc, story) => (newAcc += story.fields.customfield_10008),
-        0,
-      )),
-    0,
-  ) ||
+  (stories &&
+    Object.values(stories).reduce(
+      (acc, curr) =>
+        (acc += curr.reduce(
+          (newAcc, story) => (newAcc += story.fields.customfield_10008),
+          0,
+        )),
+      0,
+    )) ||
   0 + bugs.reduce((acc, bug) => (acc += bug.fields.customfield_10008), 0) ||
   0 +
     others.reduce((acc, other) => (acc += other.fields.customfield_10008), 0) ||
